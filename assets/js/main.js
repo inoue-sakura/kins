5bf93070aa2517b1fbcf38097adc534e82f972fc
inoue-sakura <164842401+inoue-sakura@users.noreply.github.com>
2024-08-06 14:10:07 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-
+// cover imageのSwiperの設定
+const coverSwiper = new Swiper("#cover .swiper", {
+    loop: true,
+    effect: "slide",
+    fadeEffect: {
+      crossFade: true
+    },
+    autoplay: true,
+    navigation: {
+      nextEl: "#cover .swiper-button-next",
+      prevEl: "#cover .swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination", //必須
+    },
+    allowTouchMove: false
+  });
